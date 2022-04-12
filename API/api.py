05588f435ca4dbdcb8be80c6ee5aac859deea274
MrMustard-79 <kristofferjonhendricks@gmail.com>
2022-04-11 23:54:00 -0400

    add AI for random quote

--- API/api.py
+++ API/api.py
@@ -4,6 +4,13 @@ import pymongo
 import json
 from bson.objectid import ObjectId
 
+import os
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+import numpy as np
+import pickle
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -73,10 +80,44 @@ class QuotesByKeyword(Resource):
         )
         return response
 
+# Return AI generated quote #
+
+class RandomGeneratedQuote(Resource):
+    def get(self):
+        model = load_model(os.path.join("./models/", "quote_model.h5"))
+        with open('models/tokenizer.pickle', 'rb') as handle:
+            tokenizer = pickle.load(handle)
+
+        max_sequence_len = 65
+        seed_text = "Live life like"
+        next_words = 20
+
+        for _ in range(next_words):
+            token_list = tokenizer.texts_to_sequences([seed_text])[0]
+            token_list = pad_sequences([token_list], maxlen=max_sequence_len - 1, padding='pre')
+            predicted = np.argmax(model.predict(token_list), axis=-1)
+            output_word = ""
+            for word, index in tokenizer.word_index.items():
+                if index == predicted:
+                    output_word = word
+                    break
+            seed_text += " " + output_word
+        print(seed_text)
+        quote = {"_id":0, "author": "user", "text": seed_text}
+        response = Response(
+            response= json.dumps(seed_text),
+            status=200,
+            mimetype="application/json"
+        )
+        return response
+
+
+# Add api resources #
 api.add_resource(QuotesByName, "/quotesbyauthor/<string:author>")
 api.add_resource(QuotesByKeyword, "/quotesbykeyword/")
 api.add_resource(QuotesByCategory, "/quotesbycategory/<string:category>")
 api.add_resource(OneQuote, "/onequote/")
+api.add_resource(RandomGeneratedQuote, "/random/")
 
 
 if __name__ == "__main__":
